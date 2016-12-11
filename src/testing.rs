39cfb0b835eda12cf216be7d6f57c242c12ba44b
Jim Blandy <jimb@red-bean.com>
2016-12-10 23:01:48 -0800

    Make parse test failures more legible.

--- src/testing.rs
+++ src/testing.rs
@@ -8,19 +8,20 @@
 macro_rules! assert_parse {
     ($nonterm:ty : $input:expr => Ok($ex_value:expr, $ex_tail:expr)) => {
         let input = $input as &[u8];
+        let input_printable = String::from_utf8_lossy(input).into_owned();
         let ex_value = $ex_value;
         let ex_tail = $ex_tail as &[u8];
         match <$nonterm>::parse(IndexStr::from(input)) {
             Err(e) => panic!("Parsing {:?} as {} failed: {}",
-                             input, stringify!($nonterm), e),
+                             input_printable, stringify!($nonterm), e),
             Ok((value, tail)) => {
                 if value != ex_value {
                     panic!("Parsing {:?} as {} produced {:?}, expected {:?}",
-                           input, stringify!($nonterm), value, ex_value);
+                           input_printable, stringify!($nonterm), value, ex_value);
                 }
                 if tail != ex_tail {
                     panic!("Parsing {:?} as {} left a tail of {:?}, expected {:?}",
-                           input, stringify!($nonterm), tail.as_ref(), ex_tail);
+                           input_printable, stringify!($nonterm), tail.as_ref(), ex_tail);
                 }
             }
         }
