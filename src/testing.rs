785eafaddfebe1efae4793b2bed418dd2df71185
Jim Blandy <jimb@red-bean.com>
2016-12-10 23:02:09 -0800

    Add support to assert_parse for testing parse failures.

--- src/testing.rs
+++ src/testing.rs
@@ -1,6 +1,7 @@
 /// Try to parse something, and check the result. For example:
 ///
 ///     assert_parse!(OperatorName: "quokka" => Ok(OperatorName::Question, "okka"));
+///     assert_parse!(OperatorName: "quokka" => Err(ErrorKind::UnexpectedEnd)));
 ///
 /// This asserts that calling `OperatorName::parse` on the text `"qu"`
 /// succeeds, producing the value `OperatorName::Question`, and leaving the
@@ -23,6 +24,30 @@ macro_rules! assert_parse {
                     panic!("Parsing {:?} as {} left a tail of {:?}, expected {:?}",
                            input_printable, stringify!($nonterm), tail.as_ref(), ex_tail);
                 }
+            }
+        }
+    };
+
+    ($nonterm:ty : $input:expr => Err($ex_error:pat)) => {
+        let input = $input as &[u8];
+        let input_printable = String::from_utf8_lossy(input).into_owned();
+        match <$nonterm>::parse(IndexStr::from(input)) {
+            Err(err) => {
+                // error_chain doesn't derive PartialEq for ErrorKind,
+                // so we're stuck with this, which is ridiculous
+                match *err.kind() {
+                    $ex_error => { },
+                    _ => {
+                        panic!("Parsing {:?} as {} should fail with {},\n\
+                                failed with {:?} instead",
+                               input_printable, stringify!($nonterm), stringify!($ex_error), err.kind());
+                    }
+                }
+            }
+            Ok((value, tail)) => {
+                panic!("Parsing {:?} as {} should fail with {},\n\
+                        but succeeded with value {:?}, tail {:?}",
+                       input_printable, stringify!($nonterm), stringify!($ex_error), value, tail);
             }
         }
     }
