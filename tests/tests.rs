d878740210ff9f5f0174c2ea6bf66f1399bd2b3a
Nick Fitzgerald <fitzgen@gmail.com>
2017-05-23 09:11:46 -0700

    Demangle decimal types as libiberty does

--- tests/tests.rs
+++ tests/tests.rs
@@ -64,3 +64,5 @@ demangles!(_Z5ccc_Z5cccmmmml,
            "ccc_Z(cccmm, unsigned long, unsigned long, long)");
 demangles!(__Z3S_Z3SGffffjjjjjjjjjjzjjjjjjojjjjjjjj,
            "S_Z(SGf, float, float, float, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, ..., unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned __int128, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int, unsigned int)");
+demangles!(__Z3SGfDdedddd,
+           "SGf(decimal64, long double, double, double, double, double)");
