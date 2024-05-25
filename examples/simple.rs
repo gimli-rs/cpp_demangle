ef3cb9fae1ea4addc73c53c622b934ae793724a3
Cr0a3 <127748753+Toni-Graphics@users.noreply.github.com>
2024-05-25 15:44:21 +0200

    added simple example + fixed warning in ast.rs (#294)

---
+++ examples/simple.rs
@@ -0,0 +1,15 @@
+use cpp_demangle::Symbol;
+
+pub fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+    let mangled = b"_ZN5space3fooEibc";
+    
+    let sym = Symbol::new(&mangled[..])?;
+    let demangled = sym.to_string();
+
+    println!("{}", demangled);
+
+    assert_eq!(demangled, "space::foo(int, bool, char)");
+
+    Ok(())
+}
